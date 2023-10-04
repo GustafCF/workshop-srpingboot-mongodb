d5217ebcdfb490e409226f422caca88ad13a2736
GustafCF <g.cesarfranco7@gmail.com>
2023-10-04 15:24:25 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.nelioalves.workshopmongo.resources.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+//Indica que essa classe é responsável por tratar possíveis erros nas requisições
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+}
